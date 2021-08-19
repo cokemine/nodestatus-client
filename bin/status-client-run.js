2192dec388ce02efe2ad66dcd78b29beafcbd49b
cokemine <aptx4561@gmail.com>
2021-08-20 01:23:02 +0800

    refactor: separate out status-client-run

---
+++ bin/status-client-run.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+const path = require('path');
+
+const args = process.argv.slice(2).join(' ');
+
+try {
+  const pm2 = require('pm2');
+  pm2.connect(error => {
+    if (error) {
+      console.log(error);
+      process.exit(2);
+    }
+    pm2.start(
+      {
+        name: 'nodestatus-client',
+        script: path.resolve(__dirname, '../build/bundle.js'),
+        args
+      },
+      error => {
+        if (error) throw error.message;
+        pm2.disconnect();
+      }
+    );
+  });
+} catch (error) {
+  console.log(`[ERROR]: ${error.message || error}`);
+  console.log('Something wrong. Please check if you have installed pm2 correctly.');
+  console.log('Fallback to non-pm2 daemon');
+  require('./status-client');
+}
